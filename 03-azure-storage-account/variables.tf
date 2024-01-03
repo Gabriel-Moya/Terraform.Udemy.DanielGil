5ab1da4645d128456db1636b30fef59c1556ed87
Gabriel Moya <gabrielmoya123@gmail.com>
2024-01-03 17:50:25 -0300

    Add Azure storage account configuration

---
+++ 03-azure-storage-account/variables.tf
@@ -0,0 +1,35 @@
+variable "location" {
+  description = "The Azure Region in which all resources should be created."
+  type        = string
+  default     = "West Europe"
+}
+
+variable "account_tier" {
+  description = "The Tier to use for this Storage Account."
+  type        = string
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "The Type of replication to use for this Storage Account."
+  type        = string
+  default     = "LRS"
+}
+
+variable "resource_group_name" {
+  description = "The name of the Resource Group in which all resources should be created."
+  type        = string
+  default     = "rg-terraform-training"
+}
+
+variable "storage_account_name" {
+  description = "The name of the Storage Account to create."
+  type        = string
+  default     = "gabrielmoyastterraformtraining"
+}
+
+variable "container_name" {
+  description = "The name of the Storage Container to create."
+  type        = string
+  default     = "terraform-training"
+}
